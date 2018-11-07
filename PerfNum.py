974c2a8f8d346ac3faa4c940ce47cf67f7cc9fb5
nestorojeda <nestorojedagonzalez@gmail.com>
2018-11-07 17:15:45 +0000

    Initial commit

---
+++ PerfNum.py
@@ -0,0 +1,50 @@
+class Combinations:
+    def __init__(self, n, r):
+        self.arr = [1] * int(r)
+        self.init_value = 0
+        self.i = -1
+        self.recursion_count = 0
+        self.count = 0
+
+    def generate_combinations(self, n, r):
+        self.i += 1
+
+        if r == 0:
+            yield self.arr
+        else:
+            if self.i != 0:
+                self.init_value = self.arr[self.i - 1]
+
+            self.arr[self.i] = self.init_value + 1
+
+            for x in range(self.arr[self.i], int(n) + 1):
+                self.arr[self.i] = x
+                yield from self.generate_combinations(n, r - 1)
+        self.i -= 1
+
+
+def isPerfect(i, j):
+    n = data[i] + data[j]
+    sum1 = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum1 = sum1 + i
+    if sum1 == n:
+        print("Numero perfecto hallado")
+        print(n)
+        print("Combinando las posiciones")
+        print(index)
+    else:
+        pass
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+r = int(input('Enter r = '))
+generator = Combinations(len(data), r)
+generator = generator.generate_combinations(len(data), r)
+try:
+    while True:
+        index = next(generator)
+        isPerfect(index[0] - 1, index[1] - 1)
+except StopIteration as se:
+    print('Ejecución terminada')
