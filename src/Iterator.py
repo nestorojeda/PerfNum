bc69dd3d22d8e8987fa6d30c817f77b8fed0c974
nestorojeda <nestorojedagonzalez@gmail.com>
2018-11-27 18:46:26 +0000

    Comentarios

--- src/Iterator.py
+++ src/Iterator.py
@@ -12,12 +12,14 @@ class Iterator:
         return self
 
     def __next__(self):
-        res = self.index.copy()
+        res = self.index.copy() ## Se copia el estado actual de index para enviarlo una vez se modifique
+        ##En caso de que el elemento más significativo haya llegado a su última posición, no quedan iteraciones posibles
         if self.index[0] == (self.size - len(self.index)):
             raise StopIteration
         else:
             for i in range((len(self.index) - 1), -1, -1):
-
+                ##Busca el primer elemento en la posición menos significativa que no ha llegado a su máximo valor,
+                ##lo aumenta en una unidad y cambia los elementos del array menos significativos que él
                 if self.index[i] < (self.size - len(self.index)):
                     self.index[i] = self.index[i] + 1
                     for j in range((i + 1), len(self.index)):
